53601f78c1d009d8bf766a3fa7a06d042e4799b7
weihong13 <weihong010925@163.com>
2024-07-21 22:38:31 +0800

    QT网盘项目--服务器（监听客户端连接、协议设计）

---
+++ QT_721_NetdiskServer/mytcpsocket.cpp
@@ -0,0 +1,7 @@
+#include "mytcpsocket.h"
+
+
+MyTcpSocket::MyTcpSocket()
+{
+
+}
---
+++ QT_721_NetdiskServer/protocol.h
@@ -0,0 +1,30 @@
+#ifndef PROTOCOL_H
+#define PROTOCOL_H
+
+// 重命名 unsigned int
+typedef unsigned int uint;
+
+// 消息类型的枚举值
+enum ENUM_MSG_TYPE
+{
+    ENUM_MSG_TYPE_MIN=0, // 消息类型的最小值
+
+    // 每一种消息类型一般都有两种，一个用来进行请求，一个接收响应。
+    ENUM_MSG_TYPE_REGIST_REQUEST, // 注册用户的请求
+    ENUM_MSG_TYPE_REGIST_RESPOND, // 注册用户的响应
+
+    ENUM_MSG_TYPE_MAX=0x00ffffff, // 消息类型的最大值
+
+};
+// 协议数据单元- Protocol Data Unit
+struct PDU
+{
+     uint uiPDULen;  // 协议的总长度
+     uint uiMsgType; // 消息的类型
+     uint uiMsgLen;  // 实际的消息长度
+     char caData[64];// 参数
+     char caMsg[];   // 柔性数组，用来存放实际的消息
+
+};
+
+#endif // PROTOCOL_H
