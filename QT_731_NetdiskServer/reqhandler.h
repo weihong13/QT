c43cb9e4b9d109ff18d8844fbf510f5bd8731fa9
weihong13 <weihong010925@163.com>
2024-08-01 14:36:44 +0800

    服务器--添加好友

---
+++ QT_731_NetdiskServer/reqhandler.h
@@ -0,0 +1,35 @@
+#ifndef REQHANDLER_H
+#define REQHANDLER_H
+
+#include "protocol.h"
+
+#include <QObject>
+
+class ReqHandler : public QObject
+{
+    Q_OBJECT
+public:
+    explicit ReqHandler(QObject *parent = nullptr);
+    // 处理注册请求
+    PDU* regist();
+    // 处理登录请求
+    PDU* login(QString& loginName);
+    // 处理查找用户请求
+    PDU* findUser();
+    // 处理在线用户请求
+    PDU* onlineUser();
+
+    // 处理添加好友请求
+    PDU* addFriend();
+
+    // 处理同意添加好友请求
+    void addFriendAgree();
+
+    PDU* m_pdu;
+
+
+signals:
+
+};
+
+#endif // REQHANDLER_H
