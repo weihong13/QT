90f1f333c59ce61f872fa3b595b0fbb83619f765
weihong13 <weihong010925@163.com>
2024-08-06 18:28:14 +0800

    客户端--刷新文件

---
+++ QT_806_NetdiskClient/main.cpp
@@ -0,0 +1,17 @@
+#include "client.h"
+#include "index.h"
+
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    // Client w; // 构造函数私有化了，无法使用
+    // w.show();
+
+
+    // 使用静态成员函数 获取单例对象
+    Client::getInstance().show();
+
+    return a.exec();
+}
